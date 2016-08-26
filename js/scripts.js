b6691eb97e809e22e689b5d4e6999a92dd2320a4
Alvaro Flores <alvaroflores@Alvaros-MacBook-Pro.local>
2016-08-26 10:24:25 -0700

    added prices to pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,18 @@ var consumer = 0;
 
 function Pizza (size, topping){
 	this.size = size;
-	this.topping = topping;
+	this.toppng = topping;
 }
-Pizza.prototype.totalPrice = function () {
 
+Pizza.prototype.totalPrice = function () {
+	if (topping === "sasauge") {
+		size * .3;
+	} if (topping === "pinapple") {
+		size * .4;
+	} if (topping === "pepporini") {
+		size * .5;
+	}
+	return this.topping + "pizza";
 };
 
 
@@ -16,13 +24,17 @@ Pizza.prototype.totalPrice = function () {
 
 // Front end work
 $(document).ready(function(){
-	$("form").submit(function(){
+	$("form").submit(function(event){
 		event.preventDefault();
 		var small = new Pizza(8)
 		var medium = new Pizza(12)
 		var large = new Pizza(14)
 		var pizzaSize = $("#sizes").val();
 		var toppings = $("input:radio[name=topping]:checked").val();
+
+		var newPizza = new Pizza(pizzaSize, toppings);
+
+		$("#order").text(pizzaSize + toppings);
 
 	});
 });
