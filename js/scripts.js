05e333936f446cb2297e0b5aff6fc5534c753ed5
Alvaro Flores <alvaroflores@Alvaros-MacBook-Pro.local>
2016-08-26 13:22:11 -0700

    fixed somebugs to site

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,40 @@
-function Pizza (size, topping, price){
-	debugger;
+function Pizza (size, topping){
+	// debugger;
 	this.size = size;
 	this.topping = topping;
-	this.price = price;
 }
 
-Pizza.prototype.fullOrder = function () {
-	return this.topping + " " + "Pizza";
-};
+Pizza.prototype.fullPrice = function () {
+}
 
+	var small = new Pizza(10)
+	var medium = new Pizza(12)
+	var large = new Pizza(14)
 
 
 // Front end work
 $(document).ready(function(){
-	$("form").submit(function(event){
-		event.preventDefault();
-		var small = new Pizza(8)
-		var medium = new Pizza(12)
-		var large = new Pizza(14)
-		var pizzaSize = $("#sizes").val();
-		var toppings = $("input:radio[name=topping]:checked").val();
-		var newPizza = new Pizza(pizzaSize, toppings);
-		var cost = new Pizza(pizzaSize * toppings);
-		$("ul#order").append("<li><span class = 'order'>" + newPizza.fullOrder()  + "</span></li>");
-		  $('.order').last().click(function(){
+		$("form").submit(function(event){
+			event.preventDefault();
+			var pizzaSize = $("#sizes").val();
+			var toppings = $("input:radio[name=topping]:checked").val();
+			var cost = pizzaSize;
+			if (cost === "Small"){
+				cost = small;
+			} else if (cost === "Medium") {
+					cost = medium;
+			} else {
+				cost = large;
+			}
+			var pizza = new Pizza (pizzaSize, toppings);
+
+			$("ul#order").append("<li><span class = 'order'>" + pizza.topping + " " + "Pizza" + "</span></li>");
+			$('.order').last().click(function(){
 				$("#show-order").show();
-				$('#show-order h2').text(newPizza.topping + " " + "Pizza");
-				$('.pizza').text(newPizza.size);
-				$('.topping').text(newPizza.topping);
-
-
-				$('.total').text(cost);
-
+				$('#show-order h2').text(pizza.topping + " " + "Pizza");
+				$('.pizza').text(pizza.size);
+				$('.topping').text(pizza.topping);
+				$('.total').text(pizza.fullPrice());
 		});
 	});
 });
