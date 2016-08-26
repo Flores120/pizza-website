35b28fd1f99472347d7b27d3b4231772801e8e80
Alvaro Flores <alvaroflores@Alvaros-MacBook-Pro.local>
2016-08-26 14:51:33 -0700

    added all the prices to the pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,27 @@
+
+
 function Pizza (size, topping){
-	// debugger;
 	this.size = size;
 	this.topping = topping;
 }
 
-Pizza.prototype.fullPrice = function () {
+Pizza.prototype.fullPrice = function (price) {
+	// debugger;
+			if (this.size === "Small"){
+				this.size = 10;
+	} 	if (this.size === "Medium") {
+				this.size = 12;
+	} 	if (this.size === "Large") {
+				this.size = 14;
+	}
+ 		if (this.topping === "Pepperoni"){
+			return this.size * 1.2;
+		} if (this.topping === "Sausage") {
+				return this.size * 1.3;
+		} if (this.topping === "Pineapple") {
+				return this.size * 1.4;
+		}
 }
-
-	var small = new Pizza(10)
-	var medium = new Pizza(12)
-	var large = new Pizza(14)
 
 
 // Front end work
@@ -18,14 +30,14 @@ $(document).ready(function(){
 			event.preventDefault();
 			var pizzaSize = $("#sizes").val();
 			var toppings = $("input:radio[name=topping]:checked").val();
-			var cost = pizzaSize;
-			if (cost === "Small"){
-				cost = small;
-			} else if (cost === "Medium") {
-					cost = medium;
-			} else {
-				cost = large;
-			}
+			// var cost = toppings;
+			// if (cost === "Sausage"){
+			// 	cost = 1.2;
+			// } else if (cost === "Pineapple") {
+			// 		cost = 1.3;
+			// } else {
+			// 	cost = 1.4;
+			// }
 			var pizza = new Pizza (pizzaSize, toppings);
 
 			$("ul#order").append("<li><span class = 'order'>" + pizza.topping + " " + "Pizza" + "</span></li>");
@@ -34,7 +46,7 @@ $(document).ready(function(){
 				$('#show-order h2').text(pizza.topping + " " + "Pizza");
 				$('.pizza').text(pizza.size);
 				$('.topping').text(pizza.topping);
-				$('.total').text(pizza.fullPrice());
+				$('.total').text("$" + pizza.fullPrice().toFixed(2));
 		});
 	});
 });
