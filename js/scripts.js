119a47846bd7e0c8df820decd81bb440cfd433de
Alvaro Flores <alvaro.flores01@gmail.com>
2017-04-11 13:51:13 -0700

    makes code more dry

--- js/scripts.js
+++ js/scripts.js
@@ -4,65 +4,63 @@ function Pizza (size, topping){
 }
 
 Pizza.prototype.fullPrice = function (price) {
-			if (this.size === "Small"){
-				this.size = 10;
+	if (this.size === "Small"){
+		this.size = 10;
 	} 	if (this.size === "Medium") {
-				this.size = 12;
+		this.size = 12;
 	} 	if (this.size === "Large") {
-				this.size = 14;
+		this.size = 14;
 	}
- 		if (this.topping === "Pepperoni"){
-			return this.size * 1.2;
-		} if (this.topping === "Sausage") {
-				return this.size * 1.3;
-		} if (this.topping === "Pineapple") {
-				return this.size * 1.4;
-		}
+	if (this.topping === "Pepperoni"){
+		return this.size * 1.2;
+	} if (this.topping === "Sausage") {
+		return this.size * 1.3;
+	} if (this.topping === "Pineapple") {
+		return this.size * 1.4;
+	}
 }
+
 var arr = [];
 var sum = 0;
-
 $(document).ready(function(){
-		$("form").submit(function(event){
-			event.preventDefault();
-			var pizzaSize = $("#sizes").val();
-			var toppings = $("input:radio[name=topping]:checked").val();
-			var pizza = new Pizza (pizzaSize, toppings);
-			var price = pizza.fullPrice();
-			var join = arr.push(price);
-			sum = 0;
-			for(var i= 0 ; i < arr.length; i++){
-				sum += arr[i];
-			}
-			var cost = sum;
-			console.log(cost);
-			document.getElementById("cost").innerHTML = "$" + cost.toFixed(2);
+	$("form").submit(function(event){
+		event.preventDefault();
+		var pizzaSize = $("#sizes").val();
+		var toppings = $("input:radio[name=topping]:checked").val();
+		var pizza = new Pizza (pizzaSize, toppings);
+		var price = pizza.fullPrice();
+		var join = arr.push(price);
+		sum = 0;
+		for(var i= 0 ; i < arr.length; i++){
+			sum += arr[i];
+		}
+		var cost = sum;
+		document.getElementById("cost").innerHTML = "$" + cost.toFixed(2);
+		if (pizza.topping === undefined) {
+			alert("Please select a pizza")
+		} else {
+			document.getElementById('orderIn').style.background = "rgba(0, 0, 0, .8)"
+			document.getElementById('yourorder').innerHTML = "Your Order";
+			$("ul#order").append("<li><span class = 'order'>" + pizza.topping + " " + "Pizza" + "</span></li>");
+		}
+		$('.order').last().click(function(){
+			$("#show-order").show(1000);
 			if (pizza.topping === undefined) {
-				alert("Please select a pizza")
+
 			} else {
-				document.getElementById('orderIn').style.background = "rgba(0, 0, 0, .8)"
-				document.getElementById('yourorder').innerHTML = "Your Order";
-				$("ul#order").append("<li><span class = 'order'>" + pizza.topping + " " + "Pizza" + "</span></li>");
-
+				$('#show-order h2').text(pizza.topping + " " + "Pizza");
+				$('.pizza').text(pizza.size);
+				$('.topping').text(pizza.topping);
+				$('.total').text("$" + pizza.fullPrice().toFixed(2));
 			}
-			$('.order').last().click(function(){
-				$("#show-order").show(1000);
-				if (pizza.topping === undefined) {
 
-				} else {
-					$('#show-order h2').text(pizza.topping + " " + "Pizza");
-					$('.pizza').text(pizza.size);
-					$('.topping').text(pizza.topping);
-					$('.total').text("$" + pizza.fullPrice().toFixed(2));
-				}
-
-				if (pizza.topping === "Pineapple") {
-					$("#image").html('<img src="./img/pineapple.png" />');
-				} else if (pizza.topping === "Pepperoni") {
-					$("#image").html('<img src="./img/pepperoni.png" />');
-				} else {
-					$("#image").html('<img src="./img/sausage.png" />');
-				}
+			if (pizza.topping === "Pineapple") {
+				$("#image").html('<img src="./img/pineapple.png" />');
+			} else if (pizza.topping === "Pepperoni") {
+				$("#image").html('<img src="./img/pepperoni.png" />');
+			} else {
+				$("#image").html('<img src="./img/sausage.png" />');
+			}
 		});
 	});
 });
